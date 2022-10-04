c4ba5d67ef9438b3217de32e1d3b384aeafb6e07
elvinHeo <shinilheo0205@gmail.com>
2022-10-04 23:06:38 +0900

    Initial Commit

---
+++ Dicee two/Dicee_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_twoApp.swift
+//  Dicee two
+//
+//  Created by ShinIl Heo on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
